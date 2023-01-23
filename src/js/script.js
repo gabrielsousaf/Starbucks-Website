42ff4318aca6ebd69e79a717d5e14b840f395502
gabrielsousaf <gs294860@gmail.com>
2023-01-23 19:37:06 -0300

    Update Final

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,11 @@ function changeCircleColor(color){
     const circle = document.querySelector('.circle');
     circle.style.background = color;
 }
+
+let menu = document.querySelector('#menu-icon');
+let navabr = document.querySelector('.navigation');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navabr.classList.toggle('open');
+}
